7ed5796452bc727daef5b36786f07b981ffd46a7
Joener Münch <joener.munch@gmail.com>
2020-01-13 15:24:47 -0300

    added more styling

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const aboutButton = document.querySelector(".about-btn");
 const aboutSection = document.querySelector(".about");
+const musicBtn = document.querySelector(".music");
 
 aboutButton.addEventListener("click", () => {
   aboutSection.classList.toggle("on");
+});
+
+musicBtn.addEventListener("click", () => {
+  musicBtn.classList.add("event");
 });
